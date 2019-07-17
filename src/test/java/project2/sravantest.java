deb4406d44cfd55600bdc61a53b8d4795f9bf1a0
sudheer <sudheer.ganatech308@gmail.com>
2019-07-17 10:12:16 +0530

    1st commit

---
+++ src/test/java/project2/sravantest.java
@@ -0,0 +1,17 @@
+package project2;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class sravantest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+	
+	}
+
+}
